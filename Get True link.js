d43a5fb48bdb592a2e92f01c08d7a83fe700d591
めがねこ <29650686+MeGaNeKoS@users.noreply.github.com>
2019-11-28 08:11:53 +0700

    Update Get True link.js

--- Get True link.js
+++ Get True link.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Get True link
 // @namespace    https://github.com/MeGaNeKoS/skip-short-link
-// @version      1.4.1
+// @version      1.4.2
 // @description  Script to baypass most short link!! this script to auto decode short link when you got redirect link. save your time, save your money. any request / error put it in feedback
 // @description  you can try on this web https://www.juragan-anime.net/series/boruto-naruto-next-generations-sub-indo/ https://anoboy.video/2019/11/boruto-episode-133/
 
@@ -13,36 +13,38 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // ==/UserScript==
 
-$('a').each(function(){
-	var aaa = this.href;
-	var aab = decodeURIComponent(aaa.toString());
-	var cp1 = aab.includes('aHR0');
-	var cp2 = aab.includes('http');
-	if (cp1 == true){
-		try {
-			var bba = aab.split("aHR0")[0];
-			var bbb = aab.split(bba)[1];
-			var bbc = bbb.includes('=');
-			if (bbc == true){
-				bbb = bbb.split('=')[0];
-			}
-			var bbd = atob(bbb);
-            var bbe = bbd.replace(/([_=:;&\-\/\.\?\d\w]+(\d|\w))/g,'');
-            var bbf = bbd.replace(bbe,'');
-			$(this).attr("href", bbf);
-		}catch(err) {
-			$(this).attr("href", aaa);
-		}
-	}else if (cp2 == true){
-        	var cca = aab.split("http")[2];
-        	if (aab.includes(cca) == true){
-            		try {
-                		var ccb = aab.split("http")[1];
-               			var ccc = aab.split(ccb)[1];
-               			$(this).attr("href", ccc);
-            		}catch(err) {
-                		$(this).attr("href", aaa);
-            		}
-        	}
-	}
-});
+$(document).load(function(){ decode(); });
+$(document).ready(function(){ decode(); });
+
+function decode(){
+    $('a').each(function(){
+        var aaa = this.href;
+        var aab = decodeURIComponent(aaa.toString());
+        var cp1 = aab.includes('aHR0');
+        var cp2 = aab.includes('http');
+        if (cp1 == true){
+            try {
+                var bba = aab.split("aHR0")[0];
+                var bbb = aab.split(bba)[1];
+                var bbc = bbb.split(/[\W_]+/g)[0];
+                var bbd = atob(bbc);
+                var bbe = bbd.replace(/([_=:;&\-\/\.\?\d\w]+(\d|\w))/g,'');
+                var bbf = bbd.replace(bbe,'');
+                $(this).attr("href", bbf);
+            }catch(err) {
+                $(this).attr("href", aaa);
+            }
+        }else if (cp2 == true){
+            var cca = aab.split("http")[2];
+            if (aab.includes(cca) == true){
+                try {
+                    var ccb = aab.split("http")[1];
+                    var ccc = aab.split(ccb)[1];
+                    $(this).attr("href", ccc);
+                }catch(err) {
+                    $(this).attr("href", aaa);
+                }
+            }
+        }
+    });
+}
