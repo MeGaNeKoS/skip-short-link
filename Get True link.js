44fd3711d2f453a00f3ff9f6db55d2cfd0866e42
めがねこ <29650686+MeGaNeKoS@users.noreply.github.com>
2019-12-06 12:28:50 +0700

    Update Get True link.js

--- Get True link.js
+++ Get True link.js
@@ -1,27 +1,20 @@
 // ==UserScript==
 // @name         Get True link
 // @namespace    https://github.com/MeGaNeKoS/skip-short-link
-// @version      1.4.3
-// @description  Script to baypass most short link!! this script to auto decode short link when you got redirect link. save your time, save your money. any request / error put it in feedback
-// @description  you can try on this web https://www.juragan-anime.net/series/boruto-naruto-next-generations-sub-indo/ https://anoboy.video/2019/11/boruto-episode-133/
-
+// @version      1.5.1
+// @description  Script to baypass most short link!! this script to auto decode short link when you got redirect link. save your time, save your money. any request / error put it in feedback you can try on this web https://www.juragan-anime.net/series/boruto-naruto-next-generations-sub-indo/ https://anoboy.video/2019/11/boruto-episode-133/
 // @author       MeGaNeKo(めがねこ)
 // @match        *://*/*
 // @exclude      *//*uploaded.net/*
-// @exclude      *//*mail.google.com/*
 // @exclude      *//*okedrive.com/*
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @run-at document-start
 // ==/UserScript==
 
-var trigger = 0;
-var myVar = setInterval(myTimer, 1000);
-function myTimer() {
-    trigger++;
-    console.log(trigger);
+document.onreadystatechange = function () {
     decode();
-    if(trigger > 60){clearInterval(myVar);}
-}
+};
 function decode(){
     $('a').each(function(){
         var aaa = this.href;
@@ -46,15 +39,12 @@ function decode(){
                 try {
                     var ccb = aab.split("http")[1];
                     var ccc = aab.split(ccb)[1];
-                    if (aab.includes("google.com") == true){
-                        $(this).attr("href", ccc);
-                    }else{
-                        var ccd = ccc.split(/[^A-Za-z0-9_.:/\\-]/g)[0];
-                        $(this).attr("href", ccd);
-                    }
+                    var ccd = ccc.split(/[^A-Za-z0-9_.:/\\-]/g)[0];
+                    $(this).attr("href", ccd);
                 }catch(err) {
                     $(this).attr("href", aaa);
                 }
+
             }
         }
     });
